7f77b5d709d352fa48c027e18a9bed8b53829833
Patryk <patrykjac33@gmail.com>
2026-05-14 12:09:07 +0200

    implementacja logiki inflacji #18 i bonusu Last Chance #25

--- economy.py
+++ economy.py
@@ -30,3 +30,23 @@ class Economy:
         
         # Flaga dla bonusu "Last Chance"
         self.last_chance_active = False
+    
+    def get_item_price(self, item_name):
+        """
+        Oblicza aktualną cenę przedmiotu uwzględniając inflację i bonusy.
+        Zgodnie z Issue #18 i #25.
+        """
+        if item_name not in self.base_prices:
+            return None
+            
+        base = self.base_prices[item_name]
+        bought_count = self.items_bought.get(item_name, 0)
+        
+        # Formuła inflacji: +50% ceny bazowej za każdy kupiony przedmiot (#18)
+        current_price = base + (bought_count * 0.5 * base)
+        
+        # Bonus Last Chance: -80% ceny (czyli płacimy 20%) (#25)
+        if self.last_chance_active:
+            current_price *= 0.2
+            
+        return int(current_price)
